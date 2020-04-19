0a89fe1b68ed3df65a06a65fed94fe0ed1d23b53
FadelNasereddin <50805022+FadelNasereddin@users.noreply.github.com>
2020-04-19 18:15:19 -0400

    Added Titles and subheaders

---
+++ mainApp.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+import time 
+
+def main():
+    # Headers
+    st.title("Fadel's NLP Application")
+    st.subheader("Serving all your NLP needs here!")
+
+
+    # Sidebar
+    st.sidebar.header('About this site')
+    st.write('TODO')
+    st.sidebar.header('How to Use this site')
+    st.sidebar.info("TODO")
+
+    # Tokenization
+    showTokens = st.checkbox("Show Tokens & Lemma")
+    
+    
+    if showTokens:
+        st.subheader("Tokenize your text")
+        st.info("Tokenization is ...")
+        message = st.text_area("Enter your desired text:","Type here...")
+        submitButton = st.button("Submit")
+        if submitButton:
+            st.sucess("You have submitted your text")
+
+
+
+    # Sentiment Analysis 
+
+    # Summary
+
+
+if __name__ == '__main__':
+    main()
